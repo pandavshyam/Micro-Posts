3351fd679851b0cfa8b53af2514ebca3e775f8a7
Shyam Pandav <pandavshyam@gmail.com>
2021-06-16 03:27:22 +0530

    Fix Issue related to user controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -54,7 +54,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should redirect destroy when not logged in" do
     assert_no_difference 'User.count' do
-      delete user_path(@user)
+      delete :destroy, id: @user
     end
     assert_redirected_to login_url
   end
@@ -62,7 +62,7 @@ class UsersControllerTest < ActionController::TestCase
   test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
     assert_no_difference 'User.count' do
-      delete user_path(@user)
+      delete :destroy, id: @user
     end
     assert_redirected_to root_url
   end
