3d3195095646a954c1e9d4ba6a0c22efd4a86d93
Shyam Pandav <pandavshyam@gmail.com>
2021-06-18 04:45:01 +0530

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: 10)
   end
 
   def new
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     # Bullet.profile { @user.as_json }
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     # puts @user.attributes.to_yaml
+    redirect_to root_url and return unless @user.activated
   end
 
   def edit
